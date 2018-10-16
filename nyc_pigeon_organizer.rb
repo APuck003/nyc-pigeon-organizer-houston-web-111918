9d4f2c2b69dfe6e126823157f2cf6898267fc762
Austin Puckett <puckett_austin@yahoo.com>
2018-10-16 02:48:55 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,14 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   
-  data.each do |prop, hash|
+  data.each do |property, hash|
     hash.each do |attribute, array|
       array.each do |name|
         unless new_hash.has_key?(name)
           new_hash[name] = {}
         end
         
-        unless new_hash.has_key(property)
+        unless new_hash.has_key?(property)
           new_hash[name][property] = []
         end
         
